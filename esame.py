9de5abafb68313be704d1e74be2d80830861b536
MarcoGolly <marcogollinucci@gmail.com>
2021-02-08 22:47:13 +0000

    Definitivo. sistemati: - i commenti - i vari test - ultime cose varie (spaziatura, ordine, ecc) tolto il file data.csv tolte le righe di test che utilizzavano il file

--- esame.py
+++ esame.py
@@ -3,8 +3,8 @@ class ExamException(Exception):
   pass
 
 class CSVTimeSeriesFile:
-  #costruttore, con controllo sul nome del file 
-  def __init__(self, name):
+  #costruttore, con controllo sul nome del file
+  def __init__(self, name =""):
     try:
       assert(isinstance(name, str))
       assert(name != "")
@@ -17,69 +17,52 @@ class CSVTimeSeriesFile:
     try:
       file = open(self.name, "r")
     except:
-      raise ExamException ("cannot open the file\n")
+      raise ExamException ("cannot open the file")
     
     time_series=[]
     #nel file CSV ogni riga è scritta in questo modo: "timestamp, valore numerico"
+
     for line in file:
       try:
         tmp = line.split(",")
+        assert(len(tmp) ==2)
         timestamp = int(tmp[0])
         temperature = float(tmp[1])
-        assert(len(tmp) ==2)
-        #assert(timestamp >=0) #da vedere
-        assert(temperature > -10 and temperature <50)
+        assert(timestamp >=0) #controllo forse superfluo, verifico che i timestamps siano più recenti del 1970
         time_series.append([timestamp, temperature])
       except:
-        print('this line is not valid: "{}" '.format(tmp))
+        print('the line "{}" is not valid'.format(line))
     file.close()
 
-    #controllo i timestamp, verifico che siano ordinati e non ci siano doppioni
+    #controllo i timestamp: verifico che siano ordinati e non ci siano doppioni
     for i in range(1, len(time_series)):
-      if time_series[i][0]<=time_series[i-1][0]:
+      if time_series[i-1][0]>=time_series[i][0]:
         raise ExamException("timestamps are not valid")
+    
+    #controllo che il file non sia vuoto o privo di dati utili
+    try:
+      assert(len(time_series)!=0)
+    except:
+      raise ExamException("file contains no time series")
+      
     return time_series
-  
 
 def daily_stats(time_series):
-  try:
-    assert(isinstance(time_series, list))
-    assert(len(time_series)>0)
-    for i in time_series:
-      assert(isinstance(i, list))
-      assert(len(i) ==2)
-    for i in range(1, len(time_series)):
-      assert(time_series[i][0]>time_series[i-1][0] )
-  except:
-    raise ExamException("time_series is not valid")
-  
   #qui inserisco le epochs dei giorni (alle 00:00)
   days_epoch =[]
-  #qui inserisco le temperature, divise per giorno ( days_temperature sarà una lista di liste)
+  #qui inserisco le temperature, divise per giorno
   days_temperature =[]
+
   for data in time_series:
     day_start_epoch = data[0] - (data[0] % 86400)
-    if day_start_epoch not in days_epoch:
-      #se è la prima temperatura che trovo di un giorno, aggiungo l'epoch a days_epoch
+    if day_start_epoch not in days_epoch: #se è il primo valore del giorno
       days_epoch.append(day_start_epoch)
-      #per risolvere problemi con gli indici di days_temperature aggiungo una lista vuota
       days_temperature.append([])
     #qui aggiungo la temperatura nella lista del giorno day_start_epoch a sua volta contenuta in days_temperature
     days_temperature[days_epoch.index(day_start_epoch)].append(data[1])
-  
+
   days_result =[]
   for day_temperature in days_temperature:
     days_result.append([min(day_temperature), max(day_temperature), sum(day_temperature)/len(day_temperature)])
-  
+
   return days_result
-
-try:
-  prova = CSVTimeSeriesFile(name ="data.csv")
-
-  data= prova.get_data()
-
-  res=daily_stats(data)
-  for i in res:
-    print (i)
-except ExamException as e:
-  print(e)
